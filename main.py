e673a69f70c691e2ddc94f7f231d64386cdf87f3
konstantin <kuznetsovkm@yandex.ru>
2023-02-22 19:53:51 +0300

    пустое окно + зависимости

--- main.py
+++ main.py
@@ -6,6 +6,25 @@
 
 # -----------------------------------------------------------
 
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QStyleFactory
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+
+    def init_UI(self):
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('Painter')
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.show()
+
 
 if __name__ == '__main__':
-    pass
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Fusion'))
+    window = MainWindow()
+    app.exec_()
