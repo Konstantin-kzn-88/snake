d73eceefdba336eb38adff8a6c8d9c220f68289f
konstantin <kuznetsovkm@yandex.ru>
2023-02-22 19:59:24 +0300

    центральная иконка

--- main.py
+++ main.py
@@ -7,17 +7,24 @@
 # -----------------------------------------------------------
 
 import sys
+import os
+from pathlib import Path
+
 from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QStyleFactory
+from PySide2.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        path_ico = str(Path(os.getcwd()))
+        main_ico = QIcon(path_ico + '/ico/snake.png')
+        self.setWindowIcon(main_ico)
         self.init_UI()
 
     def init_UI(self):
         self.setGeometry(500, 500, 500, 500)
-        self.setWindowTitle('Painter')
+        self.setWindowTitle('Snake')
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         self.show()
