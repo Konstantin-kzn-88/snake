90734b2318495dab42f7fc8f7ae913b1d75be9db
konstantin <kuznetsovkm@yandex.ru>
2023-02-23 19:58:32 +0300

    первое ползание змейки

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from pathlib import Path
 
 from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QStyleFactory, QLabel
 from PySide2.QtGui import QIcon, QPixmap, QPainter, QPen, QColor
-from PySide2.QtCore import QPoint, QObject, QRunnable, Signal, QThreadPool, Qt
-
+from PySide2.QtCore import QPoint, QObject, QRunnable, Signal, QThreadPool, QThread
 
 SNAKE_DIRECTION = {16777236: 'RIGHT',
                    16777234: 'LEFT',
                    16777235: 'UP',
                    16777237: 'DOWN'}
+
 
 class WorkerSignals(QObject):
     finished = Signal()
@@ -40,16 +40,19 @@ class Worker(QRunnable):
         except Exception as e:
             self.signals.error.emit(str(e))
         else:
-            self.signals.finished.emit()
-            self.signals.result.emit(var_in_worker)
+            while True:
+                QThread.sleep(1)
+                self.signals.result.emit(var_in_worker)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.LEN_SNAKE = 30  # начальная длина змеи
-        self.POINT = (100, 100)  # точка головы змеи
+        self.POINT_X = 100
+        self.POINT_Y = 100
         self.SNAKE_DIRECTION = 'RIGHT'
+        self.SNAKE_STEP = 10
 
         self.threadpool = QThreadPool()  # Пул потоков
 
@@ -64,38 +67,40 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Snake')
 
         self.label = QLabel()
-        canvas = QPixmap(500, 500)
-        canvas.fill(QColor('green'))
-        self.label.setPixmap(canvas)
+        self.canvas = QPixmap(500, 500)
+        self.canvas.fill(QColor('green'))
+        self.label.setPixmap(self.canvas)
         self.setCentralWidget(self.label)
 
-        self.draw_something()
+
+        worker = Worker()
+        worker.signals.result.connect(self.worker_output)
+        self.threadpool.start(worker)
+
         self.show()
 
-    def draw_something(self):
-        canvas = self.label.pixmap()
-        painter = QPainter(canvas)
+    def draw_snake(self):
+        self.POINT_X = self.POINT_X + self.SNAKE_STEP
+        self.canvas = QPixmap(500, 500)
+        self.canvas.fill(QColor('green'))
+        self.label.setPixmap(self.canvas)
+
+        self.canvas = self.label.pixmap()
+        painter = QPainter(self.canvas)
         pen = QPen()
         pen.setWidth(15)
         pen.setColor(QColor('blue'))
         painter.setPen(pen)
         painter.drawLine(
-            QPoint(self.POINT[0], self.POINT[1]),
-            QPoint(self.POINT[0] - self.LEN_SNAKE, self.POINT[1])
+            QPoint(self.POINT_X, self.POINT_Y),
+            QPoint(self.POINT_X - self.LEN_SNAKE, self.POINT_Y)
         )
         painter.end()
-        self.label.setPixmap(canvas)
-
-        worker = Worker()
-        worker.signals.result.connect(self.worker_output)
-        worker.signals.finished.connect(self.worker_complete)
-        self.threadpool.start(worker)
+        self.label.setPixmap(self.canvas)
 
     def worker_output(self, s):
         print(s)
-
-    def worker_complete(self):
-        print("THREAD COMPLETE!")
+        self.draw_snake()
 
     def keyPressEvent(self, event):
         self.SNAKE_DIRECTION = SNAKE_DIRECTION[event.key()]
